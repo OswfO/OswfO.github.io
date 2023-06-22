12fdb7b7fd2a9c026243df9bd29d2b2887cefda9
Swf <31833426+Swf01@users.noreply.github.com>
2023-06-22 16:13:20 -0700

    Update script.js

--- LancerRPG/script.js
+++ LancerRPG/script.js
@@ -1,15 +1,16 @@
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
-  card.addEventListener('dragstart', dragStart);
-  card.addEventListener('dragend', dragEnd);
+  card.addEventListener('mousedown', dragStart);
+  card.addEventListener('touchstart', dragStart);
+  card.addEventListener('mouseup', dragEnd);
+  card.addEventListener('touchend', dragEnd);
 });
 
 let draggedCard = null;
 
 function dragStart(e) {
   draggedCard = e.target;
-  e.dataTransfer.setData('text/plain', e.target.id);
   e.target.classList.add('dragging');
 }
 
@@ -17,14 +18,3 @@ function dragEnd(e) {
   draggedCard = null;
   e.target.classList.remove('dragging');
 }
-
-function allowDrop(e) {
-  e.preventDefault();
-}
-
-function drop(e) {
-  e.preventDefault();
-  const cardId = e.dataTransfer.getData('text/plain');
-  const card = document.getElementById(cardId);
-  if (card && card !== draggedCard) {
-    const targetColumn = e
