1ec6b728355c52f1922ddb05fa36a5d3a3cc69f7
Swf <31833426+Swf01@users.noreply.github.com>
2023-06-22 16:21:01 -0700

    Update script.js

--- LancerRPG/script.js
+++ LancerRPG/script.js
@@ -1,20 +1,53 @@
 const cards = document.querySelectorAll('.card');
+const columns = document.querySelectorAll('.column');
 
 cards.forEach(card => {
-  card.addEventListener('mousedown', dragStart);
-  card.addEventListener('touchstart', dragStart);
-  card.addEventListener('mouseup', dragEnd);
-  card.addEventListener('touchend', dragEnd);
+  card.addEventListener('dragstart', dragStart);
+  card.addEventListener('dragend', dragEnd);
+});
+
+columns.forEach(column => {
+  column.addEventListener('dragover', dragOver);
+  column.addEventListener('dragenter', dragEnter);
+  column.addEventListener('dragleave', dragLeave);
+  column.addEventListener('drop', drop);
 });
 
 let draggedCard = null;
 
 function dragStart(e) {
   draggedCard = e.target;
+  e.dataTransfer.setData('text/plain', '');
   e.target.classList.add('dragging');
 }
 
 function dragEnd(e) {
   draggedCard = null;
   e.target.classList.remove('dragging');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('column')) {
+    e.target.classList.add('drag-over');
+  }
+}
+
+function dragLeave(e) {
+  if (e.target.classList.contains('column')) {
+    e.target.classList.remove('drag-over');
+  }
+}
+
+function drop(e) {
+  e.preventDefault();
+  e.target.classList.remove('drag-over');
+  const dropZone = e.target.querySelector('.column');
+  if (draggedCard && dropZone && draggedCard !== dropZone) {
+    dropZone.appendChild(draggedCard);
+  }
 }
