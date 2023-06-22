620096e13999cc8955e951c00bf035db8ae45b8c
Swf <31833426+Swf01@users.noreply.github.com>
2023-06-22 15:32:03 -0700

    Update script.js

--- LancerRPG/script.js
+++ LancerRPG/script.js
@@ -1,18 +1,51 @@
-// Add event listeners for drag events
 const cards = document.querySelectorAll('.card');
+const dropZones = document.querySelectorAll('.drop-zone');
 
 cards.forEach(card => {
   card.addEventListener('dragstart', dragStart);
   card.addEventListener('dragend', dragEnd);
 });
 
-// Function to handle drag start event
+dropZones.forEach(dropZone => {
+  dropZone.addEventListener('dragenter', dragEnter);
+  dropZone.addEventListener('dragover', dragOver);
+  dropZone.addEventListener('dragleave', dragLeave);
+  dropZone.addEventListener('drop', drop);
+});
+
+let draggedCard = null;
+
 function dragStart(e) {
+  draggedCard = e.target;
   e.dataTransfer.setData('text/plain', e.target.id);
   e.target.classList.add('dragging');
 }
 
-// Function to handle drag end event
 function dragEnd(e) {
+  draggedCard = null;
   e.target.classList.remove('dragging');
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragLeave(e) {
+  e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+  e.preventDefault();
+  e.target.classList.remove('drag-over');
+  const cardId = e.dataTransfer.getData('text/plain');
+  const card = document.getElementById(cardId);
+  if (card && card !== draggedCard) {
+    const dropZone = e.target;
+    dropZone.appendChild(card);
+  }
 }
